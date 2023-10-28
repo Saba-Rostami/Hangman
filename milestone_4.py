776f6439f47e9cc6570081a5038aae6262a45d5a
Saba <saba_rostami20@yahoo.com>
2023-10-28 09:03:44 +0100

    Adds docstrings to python file

--- milestone_4.py
+++ milestone_4.py
@@ -2,6 +2,23 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
+
+        """
+        Initialize a new Hangman game instance.
+
+        Args:
+            word_list (list): A list of words from which the secret word is chosen.
+            num_lives (int, optional): The number of lives or incorrect guesses allowed. Default is 5.
+
+        Attributes:
+            word_list (list): The list of words to choose from.
+            num_lives (int): The initial number of lives.
+            word (str): The secret word to be guessed.
+            word_guessed (list): A list representing the progress of guessing the word.
+            num_letters (int): The number of unique letters in the secret word.
+            list_of_guesses (list): A list to store all guessed letters.
+        """
+
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(self.word_list)
@@ -10,10 +27,26 @@ class Hangman:
         self.list_of_guesses = [] 
 
     def check_guess(self, guess):
-        guess = guess.lower()   # Converts the guessed letter to lowercase 
-        if guess in self.word:   # Checks if the letter is in the randomly chosen word
+
+        """
+        Check if the guessed letter is in the secret word and update game state.
+
+        Args:
+            guess (str): The letter guessed by the player.
+
+        Prints:
+            - "Good guess! {guess} is in the word." if the guess is correct.
+            - "Sorry, {guess} is not in the word. Try again." if the guess is incorrect.
+        
+        Updates:
+            - Modifies word_guessed to reveal correct guesses.
+            - Decreases num_lives for incorrect guesses.
+        """
+
+        guess = guess.lower()    
+        if guess in self.word:   
             print(f"Good guess! {guess} is in the word.")
-            for index in range(len(self.word)):  # replaces _ with the letter 
+            for index in range(len(self.word)):  
                 if self.word[index] == guess:
                     self.word_guessed[index] = guess
                     self.num_letters -= 1
@@ -22,18 +55,30 @@ class Hangman:
             print(f"Sorry, {guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} lives left.")
         
+
     def ask_for_input(self):
-        while True:
-            guess = input("Please enter a letter: ")    # Asks the user to enter a letter
-            if len(guess) != 1 or not guess.isalpha():   # Checks if the letter is not one single letter and is an alphabet
+
+        """
+        Prompt the player for a letter and handle the input validation and game loop.
+
+        Prints:
+            - "Invalid letter. Please, enter a single alphabetical character" for invalid input.
+            - "You already tried that letter!" for repeated guesses.
+            - "Congratulations! You guessed the word" if all letters are correctly guessed.
+            - "Sorry, you're out of lives. The word was: {word}" if the player runs out of lives.
+        """
+
+        while self.num_lives > 0:
+            guess = input("Please enter a letter: ")   
+            if len(guess) != 1 or not guess.isalpha():   
                 print("Invalid letter. Please, enter a single alphabetical character.")  
-            elif guess in self.list_of_guesses:   # Checks if the letter is in the list of already guessed letters
+            elif guess in self.list_of_guesses:  
                 print("You already tried that letter!") 
             else:
-                self.list_of_guesses.append(guess)  # Add the guess to the list of guesses.
-                self.check_guess(guess)  # Call the check_guess method with the guess as an argument.
+                self.list_of_guesses.append(guess)  
+                self.check_guess(guess) 
                 if self.num_letters == 0:
-                    break  # If all letters are guessed, exit the loop.
+                    break
 
 hangman = Hangman(["apple", "banana", "watermelon", "strawberry", "grape"])
 hangman.ask_for_input()
